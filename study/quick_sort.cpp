c56623db14884ee5bb2ae5f7bb38ff496ec794a0
supermalow <229538252@qq.com>
2021-06-10 15:22:35 +0800

    master "快速排序"

--- study/quick_sort.cpp
+++ study/quick_sort.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+// 测试样例
+// 5
+// 3 1 2 4 5
+
 const int N = 1e6 + 10;
 int n;
 int q[N];
@@ -11,27 +15,26 @@ void quick_sort(int q[], int l, int r)
     {
         return;
     }
-    int x = q[l + r >> 1], i, j;
+    int x = q[l + r >> 1], i = l - 1, j = r + 1;
     while (i < j)
     {
-        do
-            i++;
-        while (q[i] < x);
-        do
-            j--;
-        while (q[j] > x);
+        while (q[++i] < x)
+            ;
+        while (q[--j] > x)
+            ;
         if (i < j)
         {
             swap(q[i], q[j]);
         }
-        quick_sort(q, l, j);
-        quick_sort(q, j + 1, r);
     }
+    quick_sort(q, l, j);
+    quick_sort(q, j + 1, r);
 }
 
 int main()
 {
     scanf("%d", &n);
+
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &q[i]);
@@ -43,5 +46,6 @@ int main()
     {
         printf("%d", q[i]);
     }
+
     return 0;
 }

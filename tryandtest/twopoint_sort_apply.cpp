7f1e767620fba707b35aaa4570a72855aae84ee1
supermalow <229538252@qq.com>
2021-06-12 14:10:13 +0800

    master "浮点二分的应用"

--- tryandtest/twopoint_sort_apply.cpp
+++ tryandtest/twopoint_sort_apply.cpp
@@ -26,7 +26,7 @@ int main()
     double n; // 输入值
     cin >> n;
     double l = -10000, r = 10000; // 定义左右边界
-    while (r - l > 1e-8)
+    while (r - l > 1e-8)          // 因为题目要求是6个小数故得1e-8  如果要求4个小数得1e-6 差两个数量级
     {
         double mid = (l + r) / 2; // 定义下表的中间值
         if (mid * mid * mid >= n) // 不能使用 ^ 因为是这个是异或的意思不是次方
